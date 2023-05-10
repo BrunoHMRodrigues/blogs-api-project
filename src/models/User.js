34943e9cca5e3febb5dba0a036fbbe523b600545
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-05-10 17:18:01 -0300

    Feature: Requisite 02 Completed - Created Model User

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {*} DataTypes
+ * @returns
+ */
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(
+      'User',
+      {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        displayName: DataTypes.STRING,
+        email: { type: DataTypes.STRING, unique: true },
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+      },
+      {
+        timestamps: false,
+        tableName: 'users',
+        underscored: true,
+      },
+    );
+    User.associate = (models) => {
+      User.hasMany(models.blog_posts, {
+        foreignKey: 'userId',
+        as: 'blogPosts',
+      });
+    };
+    return User;
+  };
